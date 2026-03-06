38a4c4697ebb5be3983b921cb552338cd55d14ef
Manoel <manoel_jonathan@hotmail.com>
2026-03-06 19:59:13 -0300

    feat(widgets): adding content to the hero section

--- src/app/widgets/header/header.component.ts
+++ src/app/widgets/header/header.component.ts
@@ -1,24 +1,19 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from '@app/shared/ui/button/button.component';
 import { ThemeService } from '@app/core/theme.service';
 import { HlmIconComponent, provideIcons } from '@spartan-ng/ui-icon-helm';
-import { lucideMenu, lucideX, lucideMoon, lucideSun } from '@ng-icons/lucide';
-
+import { lucideMenu, lucideMoon, lucideSun } from '@ng-icons/lucide';
+import { HlmSheetImports } from '@spartan-ng/helm/sheet';
+import { BrnSheetImports } from '@spartan-ng/brain/sheet';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, HlmIconComponent, ButtonComponent],
-  providers: [provideIcons({ lucideMenu, lucideX, lucideMoon, lucideSun })],
+  imports: [CommonModule, HlmIconComponent, ButtonComponent, ...BrnSheetImports, ...HlmSheetImports],
+  providers: [provideIcons({ lucideMenu, lucideMoon, lucideSun })],
   templateUrl: './header.component.html',
 })
 export class HeaderComponent {
-  isMobileMenuOpen = signal(false);
-
-  toggleMenu() {
-    this.isMobileMenuOpen.update((value) => !value);
-  }
-
   themeService = inject(ThemeService);
 }
--- src/app/widgets/hero/hero.component.ts
+++ src/app/widgets/hero/hero.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '@app/shared/ui/button/button.component';
 
 @Component({
   selector: 'app-hero',
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.css'
 })
