db00e855ded314da380ae7ad5810275ed12cf5a2
Manoel <manoel_jonathan@hotmail.com>
2026-03-06 09:40:54 -0300

    feat(widgets and dependence): switched between lucide-angular and spantan-icon and switch icons in the header

--- src/app/widgets/header/header.component.ts
+++ src/app/widgets/header/header.component.ts
@@ -1,28 +1,24 @@
 import { Component, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LucideAngularModule, Menu, X, Moon, Sun  } from 'lucide-angular';
 import { ButtonComponent } from '@app/shared/ui/button/button.component';
 import { ThemeService } from '@app/core/theme.service';
+import { HlmIconComponent, provideIcons } from '@spartan-ng/ui-icon-helm';
+import { lucideMenu, lucideX, lucideMoon, lucideSun } from '@ng-icons/lucide';
 
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, LucideAngularModule, ButtonComponent],
+  imports: [CommonModule, HlmIconComponent, ButtonComponent],
+  providers: [provideIcons({ lucideMenu, lucideX, lucideMoon, lucideSun })],
   templateUrl: './header.component.html',
 })
 export class HeaderComponent {
   isMobileMenuOpen = signal(false);
 
-  
-  readonly MenuIcon = Menu;
-  readonly CloseIcon = X;
-  readonly Moon = Moon;
-  readonly Sun = Sun;
-
   toggleMenu() {
     this.isMobileMenuOpen.update((value) => !value);
   }
 
-  themeService = inject(ThemeService)
+  themeService = inject(ThemeService);
 }
