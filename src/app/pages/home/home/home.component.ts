b48aeb61d597b43cac2fce2109f028129f629181
Manoel <manoel_jonathan@hotmail.com>
2026-03-14 10:26:38 -0300

    feat(pages): adding a new component to the homepage, approach

--- src/app/pages/home/home/home.component.ts
+++ src/app/pages/home/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { AboutComponent } from '@app/widgets/about/about.component';
+import { ApproachComponent } from '@app/widgets/approach/approach.component';
 import { HeroComponent } from '@app/widgets/hero/hero.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HeroComponent, AboutComponent],
+  imports: [HeroComponent, AboutComponent, ApproachComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
